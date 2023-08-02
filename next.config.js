57ccf1581766fb79371a7571a6ad08f8ab434bf9
Xin Guan <xin.guan@gmail.com>
2023-08-01 20:37:49 -0400

    all legacy code checked in

--- next.config.js
+++ next.config.js
@@ -13,7 +13,8 @@ module.exports = withPWA({
       'lh3.googleusercontent.com',
       'cdn.pixabay.com',
       'p16-amd-va.tiktokcdn.com',
-      'image.shutterstock.com'
+      'image.shutterstock.com',
+      'avatars.githubusercontent.com'
     ],
   },
 })
